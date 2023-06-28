2077c5494e480a6e11fb3658331498ed48ca994a
limhada <leem1315@gmail.com>
2023-06-28 15:07:10 +0900

    코드 컴포넌트화

---
+++ src/app/Archiving.tsx
@@ -0,0 +1,63 @@
+import Image, { StaticImageData } from 'next/image';
+
+import logo_github from '../../public/logo_github.png';
+import logo_naver from '../../public/logo_naver.png';
+
+export default function Archiving() {
+  interface LinkItem {
+    title: string;
+    url: string;
+    logo: StaticImageData;
+  }
+
+  const data: LinkItem[] = [
+    {
+      title: 'github',
+      url: 'github.com/limhada',
+      logo: logo_github, // 로고 이미지 경로
+    },
+    {
+      title: 'naver blog',
+      url: 'blog.naver.com/leem95',
+      logo: logo_naver, // 로고 이미지 경로
+    },
+  ];
+
+  return (
+    <div>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+        {/* 반복되는 부분을 map 함수로 생성 */}
+        {data.map((item, index) => (
+          <a
+            key={index}
+            href={`https://${item.url}`}
+            target='_blank'
+            rel='noopener noreferrer'
+            /*
+            noopener noreferrer <a> 요소의 HTML 속성 중 하나입니다. 각 부분에 대한 설명은 다음과 같습니다:
+            rel: 링크와 연결된 문서와의 관계를 지정하는 속성입니다. noopener noreferrer는 보안 및 웹 표준을 준수하기 위해 사용되는 값입니다.
+            noopener: 새로 열린 링크가 window.opener 속성을 통해 원래 문서에 접근하지 못하도록 방지합니다. 이는 보안상의 이유로 사용됩니다.
+            noreferrer: 링크를 통해 이동할 때 HTTP 리퍼러(Referer) 헤더를 전송하지 않도록 방지합니다. 이는 사용자의 개인 정보 보호를 위해 사용됩니다.
+            따라서 rel='noopener noreferrer'는 보안 및 개인 정보 보호 측면에서 링크를 안전하게 처리하기 위해 사용되는 속성입니다.
+            */
+            className='flex items-center justify-start p-6 bg-white rounded-[10px]'
+          >
+            <Image
+              src={item.logo}
+              className='h-[30px] w-[30px] mr-5'
+              alt={`logo_${item.title}`}
+            />
+            <div className='text-start'>
+              <div className='text-base font-bold md:text-lg lg:text-xl'>
+                {item.title}
+              </div>
+              <div className='text-sm md:text-base lg:text-lg text-sky-400'>
+                {item.url}
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/Projects.tsx
@@ -0,0 +1,132 @@
+import Image /* {StaticImageData} */ from 'next/image';
+// import bg from '../../public/bg.jpeg';
+// import bg2 from '../../public/bg.jpeg';
+
+
+export default function Projects() {
+
+
+
+
+  interface Project {
+    id: number;
+    // image: StaticImageData; // 이미지파일 import 해서 사용할 때 지정하는 타입
+    image: string;
+    name: string;
+    description: string;
+  }
+
+  const projects: Project[] = [
+    {
+      id: 1,
+      // image: bg,
+      image: '/bg1.jpeg',
+      name: '프로젝트 1',
+      description: '안녕하세요! 프로젝트 1 내용입니다.',
+    },
+    {
+      id: 2,
+      // image: bg2,
+      image: '/bg2.jpeg',
+      name: '프로젝트 2',
+      description: '안녕하세요! 프로젝트 2 내용입니다.',
+    },
+    {
+      id: 1,
+      // image: bg,
+      image: '/bg1.jpeg',
+      name: '프로젝트 1',
+      description: '안녕하세요! 프로젝트 1 내용입니다.',
+    },
+    {
+      id: 2,
+      // image: bg2,
+      image: '/bg2.jpeg',
+      name: '프로젝트 2',
+      description: '안녕하세요! 프로젝트 2 내용입니다.',
+    },
+    {
+      id: 1,
+      // image: bg,
+      image: '/bg1.jpeg',
+      name: '프로젝트 1',
+      description: '안녕하세요! 프로젝트 1 내용입니다.',
+    },
+    {
+      id: 2,
+      // image: bg2,
+      image: '/bg2.jpeg',
+      name: '프로젝트 2',
+      description: '안녕하세요! 프로젝트 2 내용입니다.',
+    },
+    {
+      id: 1,
+      // image: bg,
+      image: '/bg1.jpeg',
+      name: '프로젝트 1',
+      description: '안녕하세요! 프로젝트 1 내용입니다.',
+    },
+    {
+      id: 2,
+      // image: bg2,
+      image: '/bg2.jpeg',
+      name: '프로젝트 2',
+      description: '안녕하세요! 프로젝트 2 내용입니다.',
+    },
+    {
+      id: 1,
+      // image: bg,
+      image: '/bg1.jpeg',
+      name: '프로젝트 1',
+      description: '안녕하세요! 프로젝트 1 내용입니다.',
+    },
+    {
+      id: 2,
+      // image: bg2,
+      image: '/bg2.jpeg',
+      name: '프로젝트 2',
+      description: '안녕하세요! 프로젝트 2 내용입니다.',
+    },
+  ];
+
+
+
+
+  return(
+    <div>
+
+
+
+
+       {/* FIXME: 프로젝트 내용 넣기 마우스 클릭 시 프로젝트 상세페이지 or  */}
+       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+          {projects.map((project) => (
+            <div
+              key={project.id}
+              className='overflow-hidden rounded-2xl transform transition-all duration-300 ease-linear hover:-translate-y-3'
+            >
+              <Image
+                src={project.image}
+                className='w-full h-[200px] object-cover'
+                alt='프로젝트 이미지'
+                width={150}
+                height={150}
+              />
+              <div className='p-4 bg-white'>
+                <header className='mb-2'>
+                  <h3 className='overflow-hidden font-bold text-ellipsis whitespace-nowrap'>
+                    {project.name}
+                  </h3>
+                </header>
+                <p className='text-sm text-gray-400'>{project.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+
+
+
+    </div>
+  )
+}
