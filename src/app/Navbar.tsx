13fde4819d32df921fb2c387ef5ab510dcf6ead6
limhada <leem1315@gmail.com>
2023-07-12 22:50:38 +0900

    네브바 호버 컬러 변경

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -70,7 +70,7 @@ export default function Navbar() {
         <div className='flex justify-between mb-5 '>
           <div
             onClick={handleScrollToTop}
-            className='mb-[10px] mt-[20px] my-[10px] ml-[10px] md:mt-[10px] bg-blue text-2xl md:text-4xl z-50 font-bold'
+            className='mb-[10px] mt-[20px] my-[10px] ml-[10px] md:mt-[10px] bg-blue text-2xl md:text-4xl z-50 font-bold hover:text-mycolor1'
           >
             LJJ
             <br />
@@ -82,7 +82,7 @@ export default function Navbar() {
           <FontAwesomeIcon
             icon={faBars}
             onClick={() => SetMenuBt(!menuBt)}
-            className='md:hidden mt-[20px] mr-[10px] w-[25px] h-[25px] z-50'
+            className='md:hidden mt-[20px] mr-[10px] w-[25px] h-[25px] z-50 hover:text-mycolor1 cursor-pointer'
           />
 
           {/* <div className='flex flex-row mt-3 '> */}
@@ -94,7 +94,7 @@ export default function Navbar() {
                   handleScroll(title);
                 }}
                 // FIXME: 높이 다시 설정하기
-                className='mx-[10px] my-[10px] cursor-pointer h-[30%]'
+                className='mx-[10px] my-[10px] cursor-pointer h-[30%] text-slate-500 hover:text-mycolor1 cursor-pointer'
               >
                 {title}
               </div>
@@ -111,7 +111,7 @@ export default function Navbar() {
                   SetMenuBt(!menuBt);
                 }}
                 // FIXME: 배경색 변경하기
-                className='mx-[10px] my-[10px] cursor-pointer bg-slate-200 font-bold'
+                className='mx-[10px] my-[10px] cursor-pointer font-bold hover:text-mycolor1 cursor-pointer text-slate-500' // bg-slate-200
                 style={{ width: 'max-content' }}
               >
                 {title}
@@ -127,9 +127,11 @@ export default function Navbar() {
               - 임종정 -<br /> 웹 개발자 포트폴리오
             </div> */}
             <div className='text-[20px] md:text-[40px] lg:text-[50px]'>
-              <br/>
-              꾸준함을 실천하고 몰입을 즐기며<br/>웹 프론트엔드
-              개발자로 성장을 추구하는<br/>임종정입니다.
+              <br />
+              꾸준함을 실천하고 몰입을 즐기며
+              <br />웹 프론트엔드 개발자로 성장을 추구하는
+              <br />
+              임종정입니다.
             </div>
           </div>
         </div>
