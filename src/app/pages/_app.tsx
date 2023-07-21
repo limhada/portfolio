39117035e964f9b0a4fd04593cdcac7689a70318
limhada <leem1315@gmail.com>
2023-07-22 04:22:11 +0900

    ga4 적용 - 에러해결 후 10

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -35,53 +35,29 @@
 //   );
 // }
 
+// pages/_app.tsx
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import type { AppProps } from "next/app"; // 추가: AppProps 타입 임포트
 
-
-import React, { useEffect } from 'react';
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
-import Script from 'next/script';
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) { // 수정: AppProps 타입 사용
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
+    const handleRouteChange = (url: string) => { // 타입 지정
+      window.gtag("config", "G-7KN64VCFVV", {
+        page_path: url,
+      });
     };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
 
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-      <Component {...pageProps} />
-    </>
-  );
-};
+  return <Component {...pageProps} />;
+}
 
 export default MyApp;
