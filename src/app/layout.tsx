068e4d9e414b45c9103a740919cf1f0687e8a5cc
limhada <leem1315@gmail.com>
2023-07-22 02:27:29 +0900

    🔥 ga4 적용 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 
-import { useEffect } from 'react';
-import Script from 'next/script';
-import { useRouter } from 'next/router';
-import * as gtag from './lib/gtag.js';
 
 const inter = Inter({ subsets: ['latin'] });
 // subsets는 폰트에서 사용할 문자 집합을 지정하는 옵션
@@ -26,22 +22,6 @@ export default function RootLayout({
   // RootLayout 컴포넌트는 children이라는 속성을 받아올 때 해당 속성이 JSX 요소나 React 컴포넌트를 포함한 어떤 값이라도 받아들일 수 있게 됨
   children: React.ReactNode;
 }) {
-
-
-  // GA 설정 시작
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-  // GA 설정 끝
 
 
   return (
@@ -103,24 +83,6 @@ export default function RootLayout({
           href='/favicon/favicon-96x96.png'
         />
 
-        {/* GA 설정 시작 */}
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${gtag.GA_TRACKING_ID}', {
-          page_path: window.location.pathname,
-        });
-      `
-        }}
-      />
-      {/* GA 설정 끝 */}
       </head>
       <body className={inter.className}>{children}</body>
       {/* 
